d773cbdd5b2057162e1a7efb1ed8b1ff1b491d15
mskx1n0s <>
2023-03-04 21:07:40 -0600

    Fixed requirements

---
+++ custom/bad_grades.py
@@ -0,0 +1,27 @@
+from loguru import logger
+
+from abstract.pwntarget import PwnTarget
+
+
+class RestaurantPwn(PwnTarget):
+    @classmethod
+    def main(cls, process):
+        print(process.clean(timeout=0.5).decode())
+        process.sendline(b"1")
+        print(process.clean(timeout=0.3).decode())
+
+    @classmethod
+    def input_handler(cls, process, payload: bytes):
+        pos = payload.find(b'\0')
+        expected = b'Enjoy your '
+        if pos >= 0:
+            expected += payload[:pos]
+        else:
+            expected += payload
+        process.recvline()
+        data = process.recv(len(expected))
+        if data != expected:
+            logger.warning("Input handler: received and expected data mismatch")
+
+
+
